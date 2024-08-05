0c8b3ae6b7d036d5d8973912acfddb413ee1e658
VinitSama <vinitaggarwal08072002@gmail.com>
2024-08-05 18:53:08 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                About
+            </p>
+        </div>
+        <p className='text-xl mt-20 '>
+            Software Engineer - Dynamic and result-oriented Python/ReactJS/C++ coder, knows MySQL as well. Experience in developing and
+            maintaining complicated applications, with great problem-solving skills. Good in Leadership, Communication & Listening skills that help
+            him to work well within cross-functional teams. Looking for a job/ internship to apply my technical and soft skills in pacy, innovative
+            surroundings.
+
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
